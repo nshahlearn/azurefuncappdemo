ee4ba5225ea5622bc0870add16fcc546090a37af
nshahlearn <96847617+nshahlearn@users.noreply.github.com>
2022-01-10 14:35:52 -0600

    Update HttpTrigger1.cs

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -27,7 +27,7 @@ namespace Demo.Function
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully. This is demo function1.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully. This is demo function.";
 
             return new OkObjectResult(responseMessage);
         }
